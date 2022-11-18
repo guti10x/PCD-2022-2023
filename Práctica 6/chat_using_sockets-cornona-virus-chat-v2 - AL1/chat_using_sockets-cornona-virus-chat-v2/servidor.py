12c251a4a03de4028b02b0f15feffd9d7c9b2f2d
unknown <gt104515@gmail.com>
2022-11-18 17:41:30 +0100

    Al1 apartado c

--- Práctica 6/chat_using_sockets-cornona-virus-chat-v2 - AL1/chat_using_sockets-cornona-virus-chat-v2/servidor.py
+++ Práctica 6/chat_using_sockets-cornona-virus-chat-v2 - AL1/chat_using_sockets-cornona-virus-chat-v2/servidor.py
@@ -7,8 +7,10 @@ import os
 class Servidor():
 
 	def __init__(self, host=socket.gethostname(), port=int(input("Que puerto quiere usar ? "))):
+		
 		self.clientes = []
 		self.nicknamesConectados = []
+
 		print('\nSu IP actual es : ',socket.gethostbyname(host))
 		print('\n\tProceso con PID = ',os.getpid(), '\n\tHilo PRINCIPAL con ID =',threading.currentThread().getName(), '\n\tHilo en modo DAEMON = ', threading.currentThread().isDaemon(), '\n\tTotal Hilos activos en este punto del programa =', threading.active_count())
 		self.s = socket.socket()
